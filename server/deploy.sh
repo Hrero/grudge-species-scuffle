3ade263d65b2dce0c036b736eb87d68bf5f3b62e
haozengrun <haozengrun@duiba.com.cn>
2022-07-11 13:38:17 +0800

    -

---
+++ server/deploy.sh
@@ -0,0 +1,7 @@
+#export PATH=/root/.nvm/versions/node/v8.10.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/usr/local/git/bin:/root/bin
+
+node -v
+pwd
+# npm install --production
+pm2 startOrRestart ecosystem.json
+
