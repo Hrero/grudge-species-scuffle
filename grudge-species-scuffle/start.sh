6eff677ba7cdc0d08b7a292df29682bacefc3343
haozengrun <haozengrun@duiba.com.cn>
2022-07-11 15:31:24 +0800

    add

---
+++ grudge-species-scuffle/start.sh
@@ -0,0 +1,12 @@
+scp -r /Users/haozengrun/game/grudge-species-scuffle/grudge-species-scuffle root@121.196.178.118:/home/node-service
+
+pm2 start npm --name grudge-species-scuffle -- run start
+
+cd /etc/nginx/conf.d/
+
+scp -r /Users/haozengrun/game/grudge-species-scuffle/grudge-species-scuffle/8095612_game.haozengrun.com.key root@121.196.178.118:/etc/nginx/conf.d
+
+/etc/nginx/conf.d/7998885k.key
+/etc/nginx/conf.d/7998885p.pem
+
+
